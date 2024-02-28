a3ac0646dcd814c26d46e288b70826ad826830f8
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2024-02-29 00:48:46 +0200

    Add the master credintials

--- src/components/Applicant/Signin.jsx
+++ src/components/Applicant/Signin.jsx
@@ -12,8 +12,8 @@ import { LogInEmployee } from "../../services/auth/LogInEmployee";
 
 function Signin() {
   const [role, setRole] = useState("");
-  const [email, setEmail] = useState("kareemE@mail.com");
-  const [password, setPassword] = useState("kareemE");
+  const [email, setEmail] = useState("ahmed@mail.com");
+  const [password, setPassword] = useState("ahmed123456");
   const [rem, setRem] = useState(false);
   const [wrong, setWrong] = useState(false);
 
